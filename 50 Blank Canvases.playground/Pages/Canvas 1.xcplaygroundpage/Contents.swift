fbaebaa0b8ff4e506decd940fff99e0b968f7505
Sayles Keegan <ksayles@lcs-mba3139.local>
2017-10-23 11:21:22 -0400

    Worked on design 37

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -15,20 +15,23 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 // Draw Lines
-canvas.drawLine(fromX: 250, fromY: 100, toX: 225, toY:130)
-canvas.drawLine(fromX: 250, fromY: 100, toX: 275, toY: 130)
-canvas.drawLine(fromX: 275, fromY: 130, toX: 325, toY: 130)
-canvas.drawLine(fromX: 225, fromY: 130, toX: 175, toY: 130)
-canvas.drawLine(fromX: 225, fromY: 130, toX: 250, toY: 160)
-canvas.drawLine(fromX: 275, fromY: 130, toX: 250, toY: 160)
-canvas.drawLine(fromX: 275, fromY: 130, toX: 275, toY: 175)
-canvas.drawLine(fromX: 225, fromY: 130, toX: 225, toY: 175)
-canvas.drawLine(fromX: 250, fromY: 160, toX: 260, toY: 175)
-canvas.drawLine(fromX: 250, fromY: 160, toX: 240, toY: 175)
-canvas.drawLine(fromX: 225, fromY: 175, toX: 240, toY: 175)
-canvas.drawLine(fromX: 275, fromY: 175, toX: 260, toY: 175)
-canvas.drawLine(fromX: 325, fromY: 130, toX: 325, toY: 175)
-canvas.drawLine(fromX: 175, fromY: 130, toX: 175, toY: 175)
+canvas.drawLine(fromX: 250, fromY: 100, toX: 225, toY:150)
+canvas.drawLine(fromX: 250, fromY: 100, toX: 275, toY: 150)
+canvas.drawLine(fromX: 275, fromY: 150, toX: 325, toY: 150)
+canvas.drawLine(fromX: 225, fromY: 150, toX: 175, toY: 150)
+canvas.drawLine(fromX: 225, fromY: 150, toX: 250, toY: 200)
+canvas.drawLine(fromX: 275, fromY: 150, toX: 250, toY: 200)
+canvas.drawLine(fromX: 275, fromY: 150, toX: 275, toY: 230)
+canvas.drawLine(fromX: 225, fromY: 150, toX: 225, toY: 230)
+canvas.drawLine(fromX: 260, fromY: 230, toX: 250, toY: 200)
+canvas.drawLine(fromX: 240, fromY: 230, toX: 250, toY: 200)
+
+canvas.drawLine(fromX: 325, fromY: 150, toX: 325, toY: 230)
+canvas.drawLine(fromX: 175, fromY: 150, toX: 175, toY: 230)
+canvas.drawLine(fromX: 325, fromY: 230, toX: 275, toY: 230)
+canvas.drawLine(fromX: 175, fromY: 230, toX: 225, toY: 230)
+canvas.drawLine(fromX: 260, fromY: 230, toX: 275, toY: 230)
+canvas.drawLine(fromX: 240, fromY: 230, toX: 215, toY: 230)
 /*:
  ## Add your code below
  
